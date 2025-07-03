947c0abaa5f961ad753919fea2ae0a5bcac114d4
Ayaan Pupala <35133058+Ayaan-P@users.noreply.github.com>
2025-07-03 16:28:57 -0400

    Updated AppHero.tsx

--- src/components/app/AppHero.tsx
+++ src/components/app/AppHero.tsx
@@ -105,9 +105,8 @@ const AppHero = () => {
           }}
         >
           Dytto captures your daily moments and transforms them into 
-          beautiful stories. 
-          
-          Discover patterns, get personalized insights, 
+          beautiful stories.     
+        Discover patterns, get personalized insights, 
           and relive your memories like never before.
         </motion.p>
 
