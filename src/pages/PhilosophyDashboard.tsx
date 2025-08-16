1db2b8d414be8e42a17529c9dfd55a02a894759e
Ayaan Pupala <35133058+Ayaan-P@users.noreply.github.com>
2025-08-15 20:29:40 -0400

    Updated PhilosophyDashboard.tsx

--- src/pages/PhilosophyDashboard.tsx
+++ src/pages/PhilosophyDashboard.tsx
@@ -126,3 +126,5 @@ const philosophers: PhilosopherProfile[] = [
     avatar: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&fit=crop',
   }
 ]
+
+export default PhilosophyDashboard;
