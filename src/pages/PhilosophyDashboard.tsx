7ddd5901c0678841a846ab26f598efa471e5b2e3
Ayaan Pupala <35133058+Ayaan-P@users.noreply.github.com>
2025-08-15 20:53:06 -0400

    Remove duplicate closing div tag

--- src/pages/PhilosophyDashboard.tsx
+++ src/pages/PhilosophyDashboard.tsx
@@ -522,7 +522,6 @@ const PhilosophyDashboard: React.FC = () => {
                             </div>
                           </div>
                         )}
-                        </div>
                       </>
                     );
                   })()}
